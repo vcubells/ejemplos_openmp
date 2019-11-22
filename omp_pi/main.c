831df22e5ed04d9a6ab5b756bc8f2fa9b78ed279
Vicente Cubells <vcubells@gmail.com>
2019-11-22 07:16:04 -0600

    se adiciona ejemplo de PI

---
+++ omp_pi/main.c
@@ -0,0 +1,29 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static long steps = 1000000000;
+
+int main(int argc, const char *argv[])
+{
+    int i;
+    double x;
+    double pi;
+
+    double step = 1.0 / (double)steps;
+
+    double sum = 0.0;
+    double start = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+ : sum) private(x, i)
+    for (i = 0; i < steps; i++)
+    {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    pi = step * sum;
+    double delta = omp_get_wtime() - start;
+
+    printf("PI = %.16g calculado en %.4g segundos\n", pi, delta);
+}
